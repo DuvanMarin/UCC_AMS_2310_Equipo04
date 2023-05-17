cd24fdbc91ca35438815b8493e12d95a9e92a030
DuvanMarin <duvanmaringesan10@gmail.com>
2023-05-17 13:59:38 -0500

    Testing lambda

--- src/app/Components/characters/characters.component.ts
+++ src/app/Components/characters/characters.component.ts
@@ -3,24 +3,32 @@ import { CharactersService } from 'src/app/Service/characters.service';
 
 @Component({
   selector: 'app-characters',
-  templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.css'],
+  template: `
+    <div *ngIf="data">
+      <h2>Nombres de los personajes:</h2>
+      <ul>
+        <li *ngFor="let character of data.results">
+          {{ character.name }}
+        </li>
+      </ul>
+    </div>
+  `,
+  providers: [CharactersService],
 })
 export class CharactersComponent implements OnInit {
   data: any;
   characters: any[] | undefined;
   constructor(private charactersService: CharactersService) {}
+
   ngOnInit(): void {
-    this.getCharacters();
-  }
-  getCharacters() {
-    this.charactersService
-      .getCharacters()
-      .then((response) => {
-        this.characters = response;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    this.charactersService.getCharacters().subscribe(
+      (response: any) => {
+        const body = JSON.parse(response.body);
+        this.data = body.data;
+      },
+      (error: any) => {
+        console.error('Error:', error);
+      }
+    );
   }
 }
--- src/app/Service/characters.service.ts
+++ src/app/Service/characters.service.ts
@@ -6,22 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class CharactersService {
-  private cachedDataKey = 'cachedData';
   constructor(private http: HttpClient) {}
 
-  getCharacters(): Promise<any> {
-    const cachedData = localStorage.getItem(this.cachedDataKey);
-    if (cachedData) {
-      return JSON.parse(cachedData);
-    }
+  getCharacters(): Observable<any> {
     const API_URL =
       'https://lgvu3fqtzgdltmllcyt6342qn40cqxjn.lambda-url.us-east-1.on.aws/';
-    return this.http
-      .get(API_URL)
-      .toPromise()
-      .then((response) => {
-        localStorage.setItem(this.cachedDataKey, JSON.stringify(response));
-        return response;
-      });
+    return this.http.get(API_URL);
   }
 }
