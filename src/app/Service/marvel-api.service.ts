e073d4c0da8759608217aca864f0859c454fd6f7
Desireeee <74121435+DesiAPG@users.noreply.github.com>
2023-05-24 17:01:06 -0500

    Deploy events

--- src/app/Service/marvel-api.service.ts
+++ src/app/Service/marvel-api.service.ts
@@ -8,7 +8,7 @@ import { Observable, map } from 'rxjs';
 export class MarvelAPIService {
   TIMESTAMP = 1;
   PUBLIC_KEY = '2dc2cd91d9a820fe5004bf92699210e2';
-  HASH = '430012a2b7fc44c9d50feda2f1ea4bad';
+  HASH = '693e9af84d3dfcc71e640e005bdc5e2e';
   URL_API = `https://gateway.marvel.com:443/v1/public/events?apikey=${this.PUBLIC_KEY}&hash=${this.HASH}`;
 
   constructor(private http: HttpClient) {}
